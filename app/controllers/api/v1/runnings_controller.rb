ada689786689765371ee9c9f93105eb33db0caba
wagner souza <w777s32@gmail.com>
2019-08-14 19:41:01 -0300

    remove active storage and add new routes

---
+++ app/controllers/api/v1/runnings_controller.rb
@@ -0,0 +1,11 @@
+module Api
+	module V1
+		class RunningsController < ApplicationController  
+			# Listar todas as corridas
+			def index
+				runnings = Running.order('created_at DESC');
+				render json: {status: 'SUCCESS', message:'Runnings load', data: runnings},status: :ok
+			end
+ 		end
+	end
+end
